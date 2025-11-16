960f6798cda754c253e21567a4beb7a02efef9fa
nizamike229 <nizamike229@mail.ru>
2025-11-16 11:46:44 +0100

    Add ClothingRequest and ClothingResponse entity classes

---
+++ clothingLayer/Entities/ClothingRequest.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace clothingLayer.Entities;
+
+public class ClothingRequest
+{
+    [Required]
+    [MinLength(3)]
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "The field cannot consist only of spaces")]
+    public required string Title { get; set; }
+
+    [Required]
+    [MinLength(30)]
+    [RegularExpression(@"^(?!\s*$).+", ErrorMessage = "The field cannot consist only of spaces")]
+    public required string Description { get; set; }
+    
+    [Required] public required int Price { get; set; }
+
+    [Required] public required char[] Sizes { get; set; }
+
+    [Required] public required string ImagePath { get; set; }
+}
+
+public class ClothingResponse
+{
+    [Required] public required string Title { get; set; }
+
+    [Required] [MinLength(50)] public required string Description { get; set; }
+
+    [Required] public required int Price { get; set; }
+
+    [Required] public required char[] Sizes { get; set; }
+
+    [Required] public required string ImagePath { get; set; }
+}
