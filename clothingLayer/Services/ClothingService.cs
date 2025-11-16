69627dde8e487bbad0e767acd2459c68d263f0f3
nizamike229 <nizamike229@mail.ru>
2025-11-16 12:22:40 +0100

    Enhance `ClothingService` with validation, file upload handling, and improved CRUD operations

--- clothingLayer/Services/ClothingService.cs
+++ clothingLayer/Services/ClothingService.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using clothingLayer.Context;
 using clothingLayer.Contracts;
 using clothingLayer.Entities;
@@ -9,10 +10,12 @@ namespace clothingLayer.Services;
 public class ClothingService : IClothingService
 {
     private readonly ClothingsDbContext _clothingsDbContext;
+    private readonly IWebHostEnvironment _env;
 
-    public ClothingService(ClothingsDbContext clothingsDbContext)
+    public ClothingService(ClothingsDbContext clothingsDbContext, IWebHostEnvironment env)
     {
         _clothingsDbContext = clothingsDbContext;
+        _env = env;
     }
 
     public async Task<List<ClothingResponse>> GetClothingsAsync()
@@ -24,16 +27,53 @@ public class ClothingService : IClothingService
 
     public async Task<ClothingResponse> GetByIdAsync(string id)
     {
-        var clothing = await _clothingsDbContext.Clothings.FirstOrDefaultAsync(c => c.Id == id);
+        var clothing = await _clothingsDbContext.Clothings.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
         return clothing == null ? throw new Exception("Clothing not found") : clothing.ToClothingResponse();
     }
 
-    public async Task CreateClothingAsync(ClothingRequest clothingRequest)
+    public async Task CreateClothingAsync(ClothingRequest request)
     {
+        if (await _clothingsDbContext.Clothings.FirstOrDefaultAsync(c => c.Title == request.Title) != null)
+        {
+            throw new Exception("Clothing title already exists");
+        }
+
+        var clothing = new Clothing
+        {
+            Title = request.Title,
+            Description = request.Description,
+            Sizes = JsonSerializer.Serialize(request.Sizes),
+            Price = request.Price,
+            ImagePath = await GetFilePath(request.Image),
+            Id = Guid.NewGuid().ToString()
+        };
+        _clothingsDbContext.Clothings.Add(clothing);
+        await _clothingsDbContext.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(string id)
     {
-        throw new NotImplementedException();
+        var clothing = await _clothingsDbContext.Clothings.FirstOrDefaultAsync(c => c.Id == id);
+        if (clothing == null)
+        {
+            throw new Exception("Clothing not found");
+        }
+
+        _clothingsDbContext.Clothings.Remove(clothing);
+        await _clothingsDbContext.SaveChangesAsync();
+    }
+
+    private async Task<string> GetFilePath(IFormFile file)
+    {
+        var dir = Path.Combine(_env.WebRootPath, "images");
+        Directory.CreateDirectory(dir);
+
+        var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+        var filePath = Path.Combine(dir, fileName);
+
+        await using var stream = File.Create(filePath);
+        await file.CopyToAsync(stream);
+
+        return $"/images/{fileName}";
     }
 }
