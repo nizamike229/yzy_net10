03b103b18a470e1ec08837ded2dd68d8f686d525
nizamike229 <nizamike229@mail.ru>
2025-11-16 11:47:03 +0100

    Add ClothingsDbContext with configuration for SQLite and Clothing entity

---
+++ clothingLayer/Context/ClothingsDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using clothingLayer.Entities;
+
+namespace clothingLayer.Context;
+
+public partial class ClothingsDbContext : DbContext
+{
+    public ClothingsDbContext()
+    {
+    }
+
+    public ClothingsDbContext(DbContextOptions<ClothingsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Clothing> Clothings { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=/Users/nizami/Desktop/projects/net10/clothingShop/clothingLayer/products.sqlite");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Clothing>(entity =>
+        {
+            entity.ToTable("clothings");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("string(36)")
+                .HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasColumnType("text(500)")
+                .HasColumnName("description");
+            entity.Property(e => e.ImagePath).HasColumnName("image_path");
+            entity.Property(e => e.Price).HasColumnName("price");
+            entity.Property(e => e.Sizes)
+                .HasColumnType("text(400)")
+                .HasColumnName("sizes");
+            entity.Property(e => e.Title)
+                .HasColumnType("text(100)")
+                .HasColumnName("title");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
