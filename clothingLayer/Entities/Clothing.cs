69503092e21de10d8649a44860d5ceebdd794ed5
nizamike229 <nizamike229@mail.ru>
2025-11-16 11:46:33 +0100

    Add Clothing entity class to project structure

---
+++ clothingLayer/Entities/Clothing.cs
@@ -0,0 +1,16 @@
+﻿namespace clothingLayer.Entities;
+
+public class Clothing
+{
+    public string Title { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public string Sizes { get; set; } = null!;
+
+    public int Price { get; set; }
+
+    public string ImagePath { get; set; } = null!;
+
+    public string Id { get; set; } = null!;
+}
