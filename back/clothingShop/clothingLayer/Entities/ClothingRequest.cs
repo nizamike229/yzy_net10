f864f73de95277af62dcac5e7959979bfe4870c0
nizamike229 <nizamike229@mail.ru>
2025-11-26 20:42:47 +0100

    Add `Id` property to `ClothingResponse` with required validation

--- back/clothingShop/clothingLayer/Entities/ClothingRequest.cs
+++ back/clothingShop/clothingLayer/Entities/ClothingRequest.cs
@@ -23,6 +23,8 @@ public class ClothingRequest
 
 public class ClothingResponse
 {
+    [Required] public required string Id { get; set; }
+
     [Required] public required string Title { get; set; }
 
     [Required] [MinLength(50)] public required string Description { get; set; }
